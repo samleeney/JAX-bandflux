8ef55b149cc34a93df024efca89724d02780cc37
Sam Leeney <sakl2@cam.ac.uk>
2025-02-05 17:47:41 +0000

    setup for pip

--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jax_supernovae",
-    version="0.1",
+    name="jax-bandflux",
+    version="0.1.0",
+    author="Samuel Alan Kossoff Leeney",
+    author_email="sakl2@cam.ac.uk",
+    description="A JAX-based package for calculating supernovae Bandfluxes",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/samleeney/JAX-bandflux",
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'jax',
-        'sncosmo',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Astronomy",
     ],
+    python_requires=">=3.8",
+    install_requires=[
+        'numpy>=1.24.0',
+        'jax>=0.4.20',
+        'jaxlib>=0.4.20',
+        'sncosmo>=2.9.0',
+        'pytest>=7.0.0',
+        'pyyaml>=6.0.0',
+        'matplotlib',
+        'tqdm',
+        'anesthetic',
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=6.0',
+            'pytest-cov>=2.0',
+            'black>=22.0',
+            'isort>=5.0',
+        ],
+    }
 )
