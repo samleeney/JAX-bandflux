6d337b0a5d821f726009e6d6e13b7043ffbfccec
Sam Leeney <sakl2@cam.ac.uk>
2025-02-06 15:06:25 +0000

    fix build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jax-bandflux",
-    version="0.1.0",
+    version="0.1.1",
     author="Samuel Alan Kossoff Leeney",
     author_email="sakl2@cam.ac.uk",
     description="A JAX-based package for calculating supernovae Bandfluxes",
