e3fcd5c9d008282d8fe0316a5aa715acb32fe34d
Sam Leeney <sakl2@cam.ac.uk>
2025-02-06 16:14:46 +0000

    fix fmin example

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jax-bandflux",
-    version="0.1.4",
+    version="0.1.5",
     author="Samuel Alan Kossoff Leeney",
     author_email="sakl2@cam.ac.uk",
     description="A JAX-based package for calculating supernovae Bandfluxes",
